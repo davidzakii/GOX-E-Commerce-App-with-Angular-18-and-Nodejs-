f6f073a451e3402072709a68b344290e8483a3d8
David Zaki <davidzaki77@gmail.com>
2024-12-29 18:54:55 +0200

    Handle products details routing for product ID in webapp

--- webapp/src/app/components/product-details/product-details.component.ts
+++ webapp/src/app/components/product-details/product-details.component.ts
@@ -1,5 +1,7 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { CustomerService } from '../../services/customer.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-product-details',
@@ -8,8 +10,31 @@ import { ActivatedRoute, Router } from '@angular/router';
   templateUrl: './product-details.component.html',
   styleUrl: './product-details.component.scss',
 })
-export class ProductDetailsComponent implements OnInit {
+export class ProductDetailsComponent implements OnInit, OnDestroy {
+  private customerService = inject(CustomerService);
   private router = inject(Router);
   private activatedRoute = inject(ActivatedRoute);
-  ngOnInit() {}
+  private subscription: Subscription[] = [];
+  ngOnInit() {
+    let sub = this.customerService.getNewProducts().subscribe({
+      next: (result) => {
+        const ids = result.map((product) => product._id);
+        this.activatedRoute.params.subscribe({
+          next: (params) => {
+            const id = params['id'];
+            if (!ids.includes(id)) {
+              this.router.navigate(['/NotFoundPage']);
+            }
+          },
+        });
+      },
+      error: (err) => {
+        console.log(err);
+      },
+    });
+    this.subscription.push(sub);
+  }
+  ngOnDestroy() {
+    this.subscription.forEach((sub) => sub.unsubscribe());
+  }
 }
